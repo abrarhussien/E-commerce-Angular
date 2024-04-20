656dd74ec1ad007370fea052a2ff42b74494bea6
Raafat1Heiba <raafatheiba6@gmail.com>
2024-04-20 09:45:39 +0200

    update

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
 
   //apiUrl: 'https://node-e-commerce-rlkh.onrender.com',
-  apiUrl: 'http://localhost:8001'
+  apiUrl: 'http://localhost:8001',
 };
