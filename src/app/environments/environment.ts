0c3b9002b81b80821e4c96eead00cf8d04c6aa50
abrarhussien <abrarhussien@gmail.com>
2024-04-27 01:11:02 +0200

    build for deploy

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
 
-  //apiUrl: 'https://node-e-commerce-rlkh.onrender.com',
-  apiUrl: 'http://localhost:8001',
+  apiUrl: 'https://node-e-commerce-rlkh.onrender.com',
+  // apiUrl: 'http://localhost:8001',
 };
